e9c3d29a9a5e002e1b3f9d0214b87d3339db661b
05kl <kristijan.lisica@edu.tbz.ch>
2025-10-20 09:25:20 +0200

    Sprache auf Deutsch ändern

--- alter.py
+++ alter.py
@@ -2,12 +2,11 @@ from datetime import datetime
 from dateutil import relativedelta
 import sys
 
-print(sys.argv[1])
 format_code = "%d.%m.%Y"
 parsed_date = datetime.strptime(sys.argv[1], format_code)
 now = datetime.now()
 
 delta = relativedelta.relativedelta(now, parsed_date)
 diff = (now - parsed_date).days
-print(delta.years, 'Years,', delta.months, 'months,', delta.days, 'days')
-print('overall', diff, 'days.')
+print('Das Alter ist', delta.years, 'Jahre', delta.months, 'Monate und', 
+      delta.days, 'Tage, das sind', diff, 'Tage')
