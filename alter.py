c7c058c7d43415801da51554d7d3561ee356d0dc
05kl <kristijan.lisica@edu.tbz.ch>
2025-10-20 09:08:46 +0200

    works with dateutil

--- alter.py
+++ alter.py
@@ -1,10 +1,13 @@
 from datetime import datetime
+from dateutil import relativedelta
 import sys
 
 print(sys.argv[1])
 format_code = "%d.%m.%Y"
 parsed_date = datetime.strptime(sys.argv[1], format_code)
 now = datetime.now()
-diff = now - parsed_date
-print(diff.years)
 
+delta = relativedelta.relativedelta(now, parsed_date)
+diff = (now - parsed_date).days
+print(delta.years, 'Years,', delta.months, 'months,', delta.days, 'days')
+print('overall', diff, 'days.')
