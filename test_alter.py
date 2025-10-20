a7809783da10faf28ca95418b1e73b1457498ff0
05kl <kristijan.lisica@edu.tbz.ch>
2025-10-20 10:52:57 +0200

    Weitere Testfälle hinzufügen

--- test_alter.py
+++ test_alter.py
@@ -5,6 +5,18 @@ class TestPrintAge(unittest.TestCase):
     def test_yesterday_date(self):
         args = ["alter_func.py", "19.10.2025"]
         self.assertEqual(printAge(args), 0)
+    def test_past_date(self):
+        args = ["alter_func.py", "06.07.2012"]
+        self.assertEqual(printAge(args), 0)
+    def test_future_date(self):
+        args = ["alter_func.py", "21.10.2025"]
+        self.assertEqual(printAge(args), 1)
+    def test_wrong_input(self):
+        args = ["alter_func.py", "Hallo"]
+        self.assertEqual(printAge(args), 1)
+    def test_wrong_date(self):
+        args = ["alter_func.py", "33.33.5000"]
+        self.assertEqual(printAge(args), 1)
 
 if __name__ == '__main__':
     unittest.main()
